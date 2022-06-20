84bc63a95ffb833ea086ba9745c6857782b85f2d
David Felix <davidffelix@hotmail.com>
2022-06-20 11:01:16 -0300

    Insert icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,113 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(Quizz());
+
+class Quizz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: Text(
-            'Quizz',
-          ),
-        ),
-        body: Center(
-          child: Column(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'As perguntas serão exibidas aqui.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.indigo,
+              ),
+              child: Text(
+                'Verdadeiro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão verdadeiro.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text(
+                'Falso',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //O usuário clica no botão falso.
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+/*
+pergunta1: 'O metrô é um dos meios de transporte mais seguros do mundo', verdadeiro,
+pergunta2: 'A culinária brasileira é uma das melhores do mundo.', verdadeiro,
+pergunta3: 'Vacas podem voar, assim como peixes utilizam os pés para andar.', falso,
+*/
