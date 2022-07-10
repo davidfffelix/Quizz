54e618e4784d1105d9539fc981f6eefb0826d6c3
David Felix <davidffelix@hotmail.com>
2022-07-10 12:40:40 -0300

    Remove comment

--- lib/pergunta.dart
+++ lib/pergunta.dart
@@ -2,12 +2,6 @@ class Pergunta{
   String? questao;
   bool? respostaDaQuestao;
 
-/*  Pergunta({String? questao, bool? respostaDaQuestao}) { //Construtor
-    this.questao = questao;
-    this.respostaDaQuestao = respostaDaQuestao;
-  }
-*/
-
   Pergunta({this.questao, this.respostaDaQuestao}) { //Construtor
   }
 }
